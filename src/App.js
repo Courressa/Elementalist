dc474e60e050d240061f486167e71204a7cbc371
Courressa <courressamalcolm2030@gmail.com>
2026-01-20 14:46:49 -0500

    add building 1
    Former-commit-id: a8a25a09a4ee6c01493e4baaf09d63c0365757f8

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
+import { Building_1 } from './Components/Buildings/Building_1';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas
+        shadows 
+        camera={{position: [3, 3, 3], fov: 75}}
+        gl={{
+          physicallyCorrectLights: true, // Better for PBR materials like glass
+          toneMappingExposure: 1.5  
+        }}
+      >
+        <OrbitControls />
+        <ambientLight intensity={0.8} />
+        <directionalLight 
+          position={[50, 50, 50]} 
+          intensity={1.5}
+          castShadow
+        />
+        <Environment preset="night" background />
+        <Building_1 position={[0, 0, 0]} scale={1} />
+      </Canvas>
     </div>
   );
 }
